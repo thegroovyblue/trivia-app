b48d5e84c018573f84262780412a232c9ed6d995
Joel Mitchell <joey5533a@gmail.com>
2025-10-25 05:50:27 -0500

    no problems detected but still crashing

---
+++ src/composables/useScore.js
@@ -0,0 +1,11 @@
+import { ref } from 'vue'
+
+const score = ref(0)
+
+const useScore = () => {
+    const changeScore = (value) => {
+        score.value += value
+    }
+    return { score, changeScore }
+}
+ 
